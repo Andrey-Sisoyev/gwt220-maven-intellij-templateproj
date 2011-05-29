a8297be8ef452de255e04279e1c7f42256413e2d
Andrey Sisoyev <sisoyev.andrey@yandex.ru>
2011-05-30 00:39:40 +0300

    Release of v1.0

---
+++ src/main/java/frontend/client/GreetingServiceAsync.java
@@ -0,0 +1,9 @@
+package frontend.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+public interface GreetingServiceAsync {
+    void greetServer(String name, AsyncCallback<String> async);
+}
